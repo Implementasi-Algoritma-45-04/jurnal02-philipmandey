c75e0add3d584efc1d9f119644fcbb048ed77236
philipmandey <pilipmandey222@gmail.com>
2021-10-01 15:26:11 +0800

    add git

---
+++ Jurnal02.java
@@ -0,0 +1,7 @@
+public class Jurnal02 {
+	public static void main(String[] args) {
+		System.out.println("NIM  : 6706213081");
+		System.out.println("Nama : Philip Amadeo Mandey");
+		System.out.println("Kelas: D3IF-45-04");
+	}
+}
